b4faf56a58318fdfbfe494661c4f81a8da4c8f8e
JuliChav <tc23011@ues.edu.sv>
2025-06-22 18:26:24 -0600

    hice el login

--- calculadora/urls.py
+++ calculadora/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login_view, name='login' ),
+    path('sign_in', views.inicio_sesion, name='inicio_sesion'),
+    path('sign_up', views.registro, name='registro'),
 ]
--- calculadora/views.py
+++ calculadora/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import json
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def index(request):
     return render(request, 'paginas/index.html')
+
+def login_view(request):
+    return render(request, 'paginas/login.html')
+
+def inicio_sesion(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect( 'index')
+        else:
+            return render(request, "paginas/inicio_sesion.html", {'form': form, 'error': 'credenciales incorrectas'})
+    else:
+        form = AuthenticationForm()
+    return render(request, "paginas/inicio_sesion.html")
+
+def registro(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect( 'inicio_sesion')
+        else:
+            return render(request, "paginas/registro.html", {'form': form, 'error': 'credenciales incorrectas'})
+    else:
+        form = UserCreationForm()
+
+    return render(request, "paginas/registro.html")
